98b38eca3f2f093c4eda618be9ee1ad9524b8ede
jairosantamaria <jairosantamaria@amelearning.net>
2021-03-25 06:11:03 +0000

    rev01

--- src/models.py
+++ src/models.py
@@ -8,25 +8,72 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    people_id = Column(Integer, ForeignKey('people.id'))
+    favorites = relationship(Planets)
+    favorites = relationship(People)
+    favorites = relationship(User)
 
-class Address(Base):
-    __tablename__ = 'address'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    nickname = Column(String(250), nullable=False)
+    mail = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+  
+class People(Base):
+    __tablename__ = 'people'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    height = Column(Integer(250), nullable=False)
+    mass = Column(Integer(250), nullable=False)
+    hair_color = Column(String(250), nullable=False)
+    skin_color = Column(String(250), nullable=False)
+    birth_year = Column(Integer(250), nullable=False)
+    gender = Column(String(250), nullable=False)
+    home_world = Column(String(250), nullable=False)
+    planet_id = Column(Integer(250), nullable=False)
 
-    def to_dict(self):
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    rotation_period = Column(Integer(250), nullable=False)
+    orbital_period = Column(Integer(250), nullable=False)
+    diameter = Column(Integer(250), nullable=False)
+    climate = Column(String(250), nullable=False)
+    gravity = Column(String(250), nullable=False)
+    terrain = Column(String(250), nullable=False)
+    surface_water = Column(String(250), nullable=False)
+    population = Column(Integer(250), nullable=False)
+    residents = Column(Integer(250), nullable=False)
+
+class Starships(Base):
+    __tablename__ = 'starships'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    model = Column(String(250), nullable=False)
+    manufacturer = Column(String(250), nullable=False)
+    cost_in_credits = Column(Integer(250), nullable=False)
+    lenght = Column(Integer(250), nullable=False)
+    max_atmospheric_speed = Column(Integer(250), nullable=False)
+    crew = Column(String(250), nullable=False)
+    passengers = Column(Integer(250), nullable=False)
+    cargo_capacity = Column(Integer(250), nullable=False)
+    consumables = Column(String(250), nullable=False)
+    hyperdrive_rating = Column(Integer(250), nullable=False)
+    MGLT = Column(Integer(250), nullable=False)
+    starship_class = Column(String(250), nullable=False)
+    pilots = Column(Integer(250), nullable=False)
+
+def to_dict(self):
         return {}
 
 ## Draw from SQLAlchemy base
